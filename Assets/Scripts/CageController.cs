3f621f1c6b9d0a1aff917effa8871471abe65cbb
Draknut <60214040+Draknut@users.noreply.github.com>
2024-02-29 10:12:26 +0100

    GameManager

--- Assets/Scripts/CageController.cs
+++ Assets/Scripts/CageController.cs
@@ -6,17 +6,7 @@ public class CageController : MonoBehaviour
 {
     enum PlayerGoal { Goal_1,Goal_2};
     [SerializeField] PlayerGoal goal;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ball"))
