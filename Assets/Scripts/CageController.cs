db7eeeba47e82c0ce41efa88ba892d1104b93cca
Draknut <60214040+Draknut@users.noreply.github.com>
2024-03-01 17:47:47 +0100

    PAs envie d'expliquer

--- Assets/Scripts/CageController.cs
+++ Assets/Scripts/CageController.cs
@@ -1,35 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CageController : MonoBehaviour
 {
-    GameController gameController;
-    HUDController hudController;
+    [SerializeField] GameController gameController;
+    [SerializeField] HUDController hudController;
     enum PlayerGoal { Goal_1,Goal_2};
     [SerializeField] PlayerGoal goal;
 
     private void Awake()
     {
-        gameController = GameObject.Find("GameManager").GetComponent<GameController>();
-        hudController = gameController.hudController;
+        gameController = GameController.GetInstance();
+        hudController = HUDController.GetInstance();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
-            if(goal == PlayerGoal.Goal_1)
-            {
-                hudController.textGoal.SetText($"{other.GetComponent<BallController>().latesPlayerHit.GetComponent<PlayerController>().playerName} scored a point for Player 2");
-                gameController.scoreP2++;
-            }
-            else
-            {
-                hudController.textGoal.SetText($"{other.GetComponent<BallController>().latesPlayerHit.GetComponent<PlayerController>().playerName} scored a point for Player 1");
-                gameController.scoreP1++;
-            }
             hudController.textGoal.gameObject.SetActive(true);
+            ScoreGoal(other.GetComponent<BallController>().latesPlayerHit);
             StartCoroutine(WaitRestart());
         }
     }
@@ -42,5 +34,22 @@ public class CageController : MonoBehaviour
         hudController.textGoal.gameObject.SetActive(false);
         yield return new WaitForSeconds(1);
         GameController.GetInstance().RoundStart();
+    }
+
+    private void ScoreGoal(GameObject player)
+    {
+
+        if (goal == PlayerGoal.Goal_1)
+        {
+            hudController.textGoal.GetComponent<TMPro.TMP_Text>().SetText(player.GetComponent<PlayerController>().playerName + "scored a point for Team 2");
+            gameController.scoreP2++;
+            hudController.scoreP2.GetComponent<TMPro.TMP_Text>().SetText(gameController.scoreP2.ToString());
+        }
+        else
+        {
+            hudController.textGoal.GetComponent<TMPro.TMP_Text>().SetText(player.GetComponent<PlayerController>().playerName + "scored a point for Team 1");
+            gameController.scoreP1++;
+            hudController.scoreP1.GetComponent<TMPro.TMP_Text>().SetText(gameController.scoreP1.ToString());
+        }
     }
 }
